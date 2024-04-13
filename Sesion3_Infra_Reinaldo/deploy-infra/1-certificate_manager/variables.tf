de6e24cae1be8455abb94e34142497474b87f80e
lazzari89 <61710814+lazzari89@users.noreply.github.com>
2024-04-13 12:45:26 +0200

    Revert "Grupo1 - Añadiendo workflows de certificados. (#22)" (#24)
    This reverts commit 2f388d253b66d9ac1218c5f9488f64c485fa96d8.

--- Sesion3_Infra_Reinaldo/deploy-infra/1-certificate_manager/variables.tf
+++ Sesion3_Infra_Reinaldo/deploy-infra/1-certificate_manager/variables.tf
@@ -2,7 +2,7 @@
 ## REGION ##
 ############
 variable "region" {
-  default = "eu-west-3"
+  default = "eu-west-1"
 }
 ##########################################################
 ## DEFINE THE NAME APROVISIONET FOR CERTIFICATE MANAGER ##
